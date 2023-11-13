0aa5f06c64dab819a04ee6f489dff7adc5ffa0ad
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2023-11-13 18:40:58 +0100

    add logic input

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import axios from "axios";
 const App = () => {
 
   const [mealsData, setMealsData] = useState([])
+  const [inputSearch , setInputSearch] = useState("")
 
   useEffect(() =>{
-    axios.get(`https:/www.themealdb.com/api/json/v1/1/search.php?s=chicken`)
+    axios.get(`https:/www.themealdb.com/api/json/v1/1/search.php?s=` + inputSearch)
     .then ( (res) => setMealsData(res.data.meals))
-  })
+  }, [inputSearch]);
   
   return (
     <div className="app-container">
       <h1>React cooking App</h1>
-      <input type="text" placeholder="Tapez le nom d'un aliment (en anglais)" />
+      <input type="text" placeholder="Tapez le nom d'un aliment (en anglais)"  onChange={(e) => setInputSearch(e.target.value)}/>
       <div className="meals-container">
-        {mealsData.map((meal) => (
-        <Card key={meal.idMeal} meal={meal} />
+        {mealsData &&
+        mealsData
+          .slice(0 , 24)
+          .map((meal) => (
+          <Card key={meal.idMeal} meal={meal} />
         ))}
       </div>
     </div>
